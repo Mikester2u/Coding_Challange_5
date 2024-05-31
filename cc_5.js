d25d45866198273d32186f6565e9b8d2db566aea
Michael Rueda <michael.a.rueda22@gmail.com>
2024-05-31 14:37:04 -0400

    Create Manager subclass with bonus calculation

--- cc_5.js
+++ cc_5.js
@@ -12,3 +12,20 @@ class Employee { //Employee class with constructor console logging the name and 
         return this.salary * 12;
     }
 }
+
+class Manager extends Employee {
+    constructor (name, salary, department) {
+        super(name, salary);
+        this.department = department;
+    }
+
+   annualSalary() {
+    const baseAnnualSalary = super.annualSalary();
+    const bonus = baseAnnualSalary * 0.15;
+    const totalAnnualSalary = baseAnnualSalary + bonus;
+
+    console.log("Bonus for " + name + ", " + bonus );
+    console.log("Total Annual Salary for " + name + "including bonus, " + totalAnnualSalary);
+    return totalAnnualSalary;
+   } 
+}
